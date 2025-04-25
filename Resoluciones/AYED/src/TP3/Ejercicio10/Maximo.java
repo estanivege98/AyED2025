fbf35d53a73df2419cdd6178aca49666182e3ca2
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2025-04-25 12:23:57 -0300

    Resuelto y probado, de todas formas se debe revisar

---
+++ Resoluciones/AYED/src/TP3/Ejercicio10/Maximo.java
@@ -0,0 +1,24 @@
+package TP3.Ejercicio10;
+
+public class Maximo {
+    private int maximo;
+
+
+    public Maximo() {
+        this.maximo = Integer.MIN_VALUE;
+    }
+
+    public Maximo(int maximo) {
+        this.maximo = maximo;
+    }
+
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public void setMaximo(int maximo) {
+        this.maximo = maximo;
+    }
+
+
+}
--- Resoluciones/AYED/src/TP3/Ejercicio10/ParcialArboles.java
+++ Resoluciones/AYED/src/TP3/Ejercicio10/ParcialArboles.java
@@ -24,33 +24,79 @@ valor 0*/
 
     public static List<Integer> resolver(GeneralTree<Integer> arbol) {
         List<Integer> lista = new ArrayList<Integer>();
+        List<Integer> listaAct = new ArrayList<Integer>();
         if (arbol.isEmpty()) {
             return lista;
         } else {
             int suma = 0;
-            recorrido(arbol, lista, suma, 0);
+            Maximo max = new Maximo();
+            recorrido(arbol, lista ,listaAct, suma, 0, max);
             return lista;
         }
 
     }
 
-        private static void recorrido (GeneralTree<Integer> arbol, List<Integer> lista, int suma, int nivel){
-            if (arbol.isLeaf()) {
-                if(arbol.getData() == 1){
-                    lista.add(arbol.getData());
-                    suma += arbol.getData() * nivel;
+        private static void recorrido (GeneralTree<Integer> arbol, List<Integer> lista,List<Integer> listaAct, int suma, int nivel, Maximo max){
+            if(arbol.getData() == 1){
+                listaAct.add(arbol.getData());
+                suma += arbol.getData() * nivel;
+            }
+            if(arbol.isLeaf()){
+                if(suma > max.getMaximo()){
+                    max.setMaximo(suma);
+                    lista.clear();
+                    lista.addAll(listaAct);
                 }
             }
             else{
-                for(GeneralTree < Integer > hijo : arbol.getChildren()){
-                    if(arbol.getData() == 1){
-                        lista.add(arbol.getData());
-                        suma += arbol.getData() * nivel;
-                    }
-                    recorrido(hijo, lista, suma, nivel+1);
+                for (GeneralTree<Integer> hijo : arbol.getChildren()) {
+                    recorrido(hijo, lista, listaAct, suma, nivel + 1, max);
                 }
-
             }
+            //listaAct.remove(listaAct.size()-1);
         }
+
+    public static void main(String[] args){
+        List<GeneralTree<Integer>> subChildren1 = new LinkedList<GeneralTree<Integer>>();
+        subChildren1.add(new GeneralTree<Integer>(1));
+        subChildren1.add(new GeneralTree<Integer>(1));
+        subChildren1.add(new GeneralTree<Integer>(1));
+        GeneralTree<Integer> subA = new GeneralTree<Integer>(1, subChildren1);
+        List<GeneralTree<Integer>> subChildren2 = new LinkedList<GeneralTree<Integer>>();
+        subChildren2.add(subA);
+        subChildren2.add(new GeneralTree<Integer>(1));
+        GeneralTree<Integer> a1 = new GeneralTree<Integer>(0, subChildren2);
+
+        List<GeneralTree<Integer>> subChildren3 = new LinkedList<GeneralTree<Integer>>();
+        subChildren3.add(new GeneralTree<Integer>(1));
+        GeneralTree<Integer> subB = new GeneralTree<Integer>(0, subChildren3);
+        List<GeneralTree<Integer>> subChildren4 = new LinkedList<GeneralTree<Integer>>();
+        subChildren4.add(subB);
+        GeneralTree<Integer> subC = new GeneralTree<Integer>(0, subChildren4);
+        List<GeneralTree<Integer>> subChildren5 = new LinkedList<GeneralTree<Integer>>();
+        subChildren5.add(new GeneralTree<Integer>(1));
+        subChildren5.add(subC);
+        GeneralTree<Integer> a2 = new GeneralTree<Integer>(1, subChildren5);
+
+        List<GeneralTree<Integer>> subChildren6 = new LinkedList<GeneralTree<Integer>>();
+        subChildren6.add(new GeneralTree<Integer>(0));
+        subChildren6.add(new GeneralTree<Integer>(0));
+        GeneralTree<Integer> subD = new GeneralTree<Integer>(0, subChildren6);
+        List<GeneralTree<Integer>> subChildren7 = new LinkedList<GeneralTree<Integer>>();
+        subChildren7.add(subD);
+        GeneralTree<Integer> subE = new GeneralTree<Integer>(0, subChildren7);
+        List<GeneralTree<Integer>> subChildren8 = new LinkedList<GeneralTree<Integer>>();
+        subChildren8.add(subE);
+        GeneralTree<Integer> a3 = new GeneralTree<Integer>(1, subChildren8);
+
+        List<GeneralTree<Integer>> arbol = new LinkedList<GeneralTree<Integer>>();
+        arbol.add(a1);
+        arbol.add(a2);
+        arbol.add(a3);
+        GeneralTree<Integer> a = new GeneralTree<Integer>(1, arbol);
+
+        System.out.println(resolver(a));
+
+    }
     }
 

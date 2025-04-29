4ed3278fa860cc93fa8237ff4edda18a426d3fe6
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2025-04-29 10:22:05 -0300

    Reimplementado por niveles

--- Resoluciones/AYED/src/TP3/Ejercicio9/ParcialArboles.java
+++ Resoluciones/AYED/src/TP3/Ejercicio9/ParcialArboles.java
@@ -2,35 +2,76 @@ package TP3.Ejercicio9;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import TP1.Ejercicio8.*;
 import TP3.Ejercicio1.GeneralTree;
 /* SE DEBE REIMPLEMENTAR EN ITERATIVO (POR NIVELES) */
 public class ParcialArboles {
+//    public static boolean esDeSeleccion(GeneralTree<Integer> arbol){
+//        if(arbol.isEmpty()){
+//            return false;
+//        }
+//        else{
+//            return seleccion(arbol, arbol.getData());
+//        }
+//    }
+//
+//    private static boolean seleccion(GeneralTree<Integer> arbol, int datoPadre){
+//
+//        if(arbol.isLeaf()){
+//            return true;
+//        }
+//        else{
+//            boolean seleccion = true;
+//            int min = Integer.MAX_VALUE;
+//            List<GeneralTree<Integer>> hijos = arbol.getChildren();
+//            Iterator<GeneralTree<Integer>> it = hijos.iterator();
+//            while (it.hasNext() && (seleccion)){
+//                GeneralTree<Integer> hijo = it.next();
+//                min = Math.min(min, hijo.getData());
+//                seleccion = seleccion(hijo, datoPadre);
+//            }
+//            return seleccion && (min >= datoPadre);
+//
+//        }
+//    }
+
+    /* Probablemente se pueda mejorar el algoritmo quitando la variable
+    * contador, pero ahora no tengo ganas jajaj */
+
     public static boolean esDeSeleccion(GeneralTree<Integer> arbol){
         if(arbol.isEmpty()){
             return false;
         }
         else{
-            return seleccion(arbol, arbol.getData());
-        }
-    }
-
-    private static boolean seleccion(GeneralTree<Integer> arbol, int datoPadre){
-
-        if(arbol.isLeaf()){
-            return true;
-        }
-        else{
+            GeneralTree<Integer> abAux = new GeneralTree<Integer>();
+            Queue<GeneralTree<Integer>> cola = new Queue<GeneralTree<Integer>>();
+            cola.enqueue(arbol);
+            cola.enqueue(null);
+            int datoPadre = arbol.getData();
             boolean seleccion = true;
-            int min = Integer.MAX_VALUE;
-            List<GeneralTree<Integer>> hijos = arbol.getChildren();
-            Iterator<GeneralTree<Integer>> it = hijos.iterator();
-            while (it.hasNext() && (seleccion)){
-                GeneralTree<Integer> hijo = it.next();
-                min = Math.min(min, hijo.getData());
-                seleccion = seleccion(hijo, datoPadre);
+            int contador = 0;
+            while(!cola.isEmpty() && seleccion){
+                abAux = cola.dequeue();
+                if(abAux != null){
+                    if(abAux.getData() == datoPadre){
+                        contador++;
+                    }
+                    for(GeneralTree<Integer> hijo : abAux.getChildren()){
+                        cola.enqueue(hijo);
+                    }
+                }
+                else{
+                    if(contador == 0 && !cola.isEmpty()){
+                        seleccion = false;
+                    }
+                    if(!cola.isEmpty()){
+                        cola.enqueue(null);
+                        contador = 0;
+                        //datoPadre = cola.dequeue().getData();
+                    }
+                }
             }
-            return seleccion && (min >= datoPadre);
-
+            return seleccion;
         }
     }
 

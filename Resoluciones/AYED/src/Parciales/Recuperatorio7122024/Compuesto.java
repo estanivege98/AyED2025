acfa99471590d369f0933dfa2aae5ba4db497c5b
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2025-05-07 19:52:33 -0300

    Para revisar

---
+++ Resoluciones/AYED/src/Parciales/Recuperatorio7122024/Compuesto.java
@@ -0,0 +1,29 @@
+package Parciales.Recuperatorio7122024;
+
+public class Compuesto {
+    private String tipo;
+    private int tiempo;
+
+
+    public Compuesto(String tipo, int tiempo) {
+        this.tipo = tipo;
+        this.tiempo = tiempo;
+
+    }
+
+    public int getVueltas() {
+        return tiempo;
+    }
+
+    public double getCompuesto(){
+        if(tipo.equals("Med")){
+            return 0.4;
+        }
+        if(tipo.equals("Hard")){
+            return 0.7;
+        }
+        return 0;
+    }
+
+
+}
---
+++ Resoluciones/AYED/src/Parciales/Recuperatorio7122024/Estrategia.java
@@ -0,0 +1,43 @@
+package Parciales.Recuperatorio7122024;
+
+import TP3.Ejercicio1.GeneralTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estrategia {
+    public List<Compuesto> mejorEstrategia(GeneralTree<Compuesto> arbol) {
+        if (arbol == null || arbol.isEmpty()) {
+            return null;
+        }
+        List<Compuesto> respuesta = new ArrayList<>();
+        List<Compuesto> camAct = new ArrayList<>();
+        int tiempo = 0;
+        int menorTiempo = Integer.MAX_VALUE;
+        helper(arbol, respuesta, camAct, tiempo, menorTiempo);
+        return respuesta;
+    }
+
+    private int helper(GeneralTree<Compuesto> arbol, List<Compuesto> respuesta, List<Compuesto> camAct, int tiempo, int menorTiempo) {
+        if (tiempo > 0) {
+            int tiempoNodo = (arbol.getData().getVueltas() * (int) arbol.getData().getCompuesto());
+            tiempo += tiempoNodo;
+            camAct.add(arbol.getData());
+            if (arbol.isLeaf()) {
+                if (tiempo > menorTiempo) {
+                    menorTiempo = tiempo;
+                    respuesta.clear();
+                    respuesta.addAll(new ArrayList<>(camAct));
+                }
+            }
+        }
+
+
+        for (GeneralTree<Compuesto> hijo : arbol.getChildren()) {
+            menorTiempo = helper(hijo, respuesta, camAct, tiempo + 10, menorTiempo);
+        }
+
+        camAct.remove(camAct.size() - 1);
+        return menorTiempo;
+    }
+}

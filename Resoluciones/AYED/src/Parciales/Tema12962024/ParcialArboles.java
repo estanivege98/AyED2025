181ae2fba097460e91a55b6e885c3f31c13825ee
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2025-05-08 12:29:15 -0300

    Ejercicio nuevo

---
+++ Resoluciones/AYED/src/Parciales/Tema12962024/ParcialArboles.java
@@ -0,0 +1,37 @@
+package Parciales.Tema12962024;
+
+import TP2.Ejercicio1.BinaryTree;
+
+public class ParcialArboles {
+    private BinaryTree<Integer> arbol;
+
+    public ParcialArboles(BinaryTree<Integer> arbol) {
+        this.arbol = arbol;
+    }
+
+    public BinaryTree<Integer> nuevoTree() {
+        if(arbol == null || arbol.isEmpty()){
+            return null;
+        }
+        BinaryTree<Integer> arbolNuevo = new BinaryTree<Integer>();
+        //arbolNuevo.addLeftChild(arbolNuevo);
+        helper(arbol, arbolNuevo, 0);
+        return arbolNuevo;
+    }
+
+    private void helper(BinaryTree<Integer> arbol, BinaryTree<Integer> arbolNuevo, int valorAnt) {
+        int valorAct = arbol.getData() + valorAnt;
+        arbolNuevo.setData(valorAct);
+        valorAnt = arbol.getData();
+        if(arbol.hasLeftChild()){
+            BinaryTree<Integer> nuevoHijoIzq = new BinaryTree<Integer>();
+            arbolNuevo.addLeftChild(nuevoHijoIzq);
+            this.helper(arbol.getLeftChild(), arbolNuevo.getLeftChild(), valorAnt);
+        }
+        if(arbol.hasRightChild()){
+            BinaryTree<Integer> nuevoHijoDer = new BinaryTree<Integer>();
+            arbolNuevo.addRightChild(nuevoHijoDer);
+            this.helper(arbol.getRightChild(), arbolNuevo.getRightChild(), 0);
+        }
+    }
+}
---
+++ Resoluciones/AYED/src/Parciales/Tema12962024/Prueba.java
@@ -0,0 +1,26 @@
+package Parciales.Tema12962024;
+import TP2.Ejercicio1.*;
+public class Prueba {
+    public static void main(String[] args) {
+        BinaryTree<Integer> arbol = new BinaryTree<Integer>(1);
+        BinaryTree<Integer> hijoIzq = new BinaryTree<Integer>(2);
+        BinaryTree<Integer> hijoDer = new BinaryTree<Integer>(3);
+        arbol.addLeftChild(hijoIzq);
+        arbol.addRightChild(hijoDer);
+
+        BinaryTree<Integer> hijoIzqIzq = new BinaryTree<Integer>(4);
+        hijoIzq.addLeftChild(hijoIzqIzq);
+
+        BinaryTree<Integer> hijoDerIzq = new BinaryTree<Integer>(5);
+        BinaryTree<Integer> hijoDerDer = new BinaryTree<Integer>(6);
+        hijoDer.addLeftChild(hijoDerIzq);
+        hijoDer.addRightChild(hijoDerDer);
+
+        BinaryTree<Integer> ultimo = new BinaryTree<Integer>(7);
+        hijoDer.getLeftChild().addLeftChild(ultimo);
+
+        ParcialArboles parcial = new ParcialArboles(arbol);
+        BinaryTree<Integer> nuevoArbol = parcial.nuevoTree();
+        nuevoArbol.entreNiveles(0, 3);
+    }
+}

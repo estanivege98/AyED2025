3eec98fb9caaec85c603d6ceaf4d616f1867eaff
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2025-05-09 12:10:10 -0300

    Parcial que está en la filmina de teoria

---
+++ Resoluciones/AYED/src/Parciales/ParcialPrincesaGenerales/ParcialArboles.java
@@ -0,0 +1,40 @@
+package Parciales.ParcialPrincesaGenerales;
+
+import TP3.Ejercicio1.GeneralTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParcialArboles {
+    public List<Personaje> encontrarPrincesa(GeneralTree<Personaje> arbol) {
+        List<Personaje> camino = new ArrayList<Personaje>();
+        List<Personaje> camActual = new ArrayList<Personaje>();
+
+        if (arbol == null || arbol.isEmpty()) {
+            return camino;
+        } else {
+            recorrido(arbol, camino, camActual);
+            return camino;
+        }
+    }
+
+    private void recorrido(GeneralTree<Personaje> arbol, List<Personaje> camino, List<Personaje> camActual) {
+        if (!arbol.getData().esDragon()) {
+            if (camino.isEmpty()) {
+                camActual.add(arbol.getData());
+                if (arbol.getData().esPrincesa()) {
+                    camino.clear();
+                    camino.addAll(new ArrayList<Personaje>(camActual));
+                }
+            }
+
+        }
+//        else{
+//            camActual.clear();
+//        }
+        for (GeneralTree<Personaje> hijo : arbol.getChildren()) {
+            recorrido(hijo, camino, camActual);
+        }
+        camActual.remove(camActual.size() - 1);
+    }
+}
---
+++ Resoluciones/AYED/src/Parciales/ParcialPrincesaGenerales/Personaje.java
@@ -0,0 +1,19 @@
+package Parciales.ParcialPrincesaGenerales;
+
+public class Personaje {
+    private String nombre;
+    private String tipo;
+
+    public Personaje(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public boolean esDragon(){
+        return tipo.equals("Dragon");
+    }
+
+    public boolean esPrincesa(){
+        return tipo.equals("Princesa");
+    }
+}

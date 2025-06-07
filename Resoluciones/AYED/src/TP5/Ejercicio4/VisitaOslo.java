436457adfade03add9d3b5e178c72eb2c37fd4aa
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2025-06-07 18:19:22 -0300

    Ejercicio 4 implementado

---
+++ Resoluciones/AYED/src/TP5/Ejercicio4/VisitaOslo.java
@@ -0,0 +1,65 @@
+/* Se quiere realizar un paseo en bicicleta por lugares emblemáticos de Oslo. Para esto se cuenta con un grafo de
+bicisendas. Partiendo desde el “Ayuntamiento” hasta un lugar destino en menos de X minutos, sin pasar por un
+conjunto de lugares que están restringidos.
+Escriba una clase llamada VisitaOslo e implemente su método:
+paseoEnBici(Graph<String> lugares, String destino, int maxTiempo, List<String> lugaresRestringidos) :
+List<String>*/
+package TP5.Ejercicio4;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import TP5.ejercicio1.*;
+
+public class VisitaOslo {
+    public List<String> paseoEnBici(Graph<String> lugares, String destino, int maxTiempo, List<String> lugaresRestringidos) {
+        List<String> recorrido = new ArrayList<String>();
+        if (lugares == null || destino == null || lugaresRestringidos == null) {
+            return recorrido; // Retorna una lista vacía si los parámetros son nulos
+        }
+        else{
+            boolean[] marcas = new boolean[lugares.getSize()];
+            Vertex<String> origen = lugares.search("Ayuntamiento");
+            Vertex<String> des = lugares.search(destino);
+            marcarRestringidos(lugares, lugaresRestringidos, marcas);
+
+            recorridoAux(lugares, origen, des, maxTiempo, lugaresRestringidos, recorrido, 0, marcas);
+        }
+        return recorrido;
+    }
+
+    private void marcarRestringidos(Graph<String> lugares, List<String> lugaresRestringidos, boolean[] marcas) {
+        for(String nom : lugaresRestringidos) {
+            Vertex<String> v = lugares.search(nom);
+            if (v != null){
+                marcas[v.getPosition()] = true;
+            }
+        }
+    }
+
+    private boolean recorridoAux(Graph<String> lugares, Vertex<String> origen, Vertex<String> destino, int maxTiempo,
+                              List<String> lugaresRestringidos, List<String> recorrido, int tiempoActual, boolean[] marcas) {
+        boolean encontrado = false;
+        marcas[origen.getPosition()] = true; // Marca el origen como visitado
+        recorrido.add(origen.getData()); // Agrega el origen al recorrido
+        if (origen == destino){
+            return true;
+        }
+        else{
+            List<Edge<String>> adyacentes = lugares.getEdges(origen);
+            for(Edge<String> adyacente : adyacentes){
+                Vertex<String> vecino = adyacente.getTarget();
+                int tiempoArista = adyacente.getWeight();
+                if(marcas[vecino.getPosition()] && tiempoActual - tiempoArista >= 0){
+                    encontrado = recorridoAux(lugares, vecino, destino, maxTiempo, lugaresRestringidos, recorrido, tiempoActual - tiempoArista, marcas);
+                }
+            }
+        }
+        if(!encontrado){
+            recorrido.remove(recorrido.size()-1);
+        }
+        marcas[origen.getPosition()] = false; // Desmarca el origen al retroceder
+        return encontrado;
+
+    }
+}

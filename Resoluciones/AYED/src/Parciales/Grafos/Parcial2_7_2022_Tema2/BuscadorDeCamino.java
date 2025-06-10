3e8607b71a63bde0a1d7eaa239ed1addd3573820
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2025-06-10 13:02:17 -0300

    Parcial resuelto para revisar

---
+++ Resoluciones/AYED/src/Parciales/Grafos/Parcial2_7_2022_Tema2/BuscadorDeCamino.java
@@ -0,0 +1,55 @@
+package Parciales.Grafos.Parcial2_7_2022_Tema2;
+import TP5.ejercicio1.*;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.LinkedList;
+public class BuscadorDeCamino {
+    public List<String> caminoConPresupuesto(Graph<String> ciudades, String origen, String destino, int montoMinimo){
+        List<String> caminoFinal = new LinkedList<String>();
+        if (ciudades != null || !ciudades.isEmpty()) {
+            Vertex<String> vOr = ciudades.search(origen);
+            if(vOr != null){
+                Vertex<String> vDestino = ciudades.search(destino);
+                if (vDestino != null){
+                    List<String> caminoActual = new LinkedList<String>();
+                    boolean[] visitados = new boolean[ciudades.getSize()];
+                    dfs(ciudades, vOr, vDestino, caminoActual, caminoFinal, visitados, montoMinimo, 0);
+                }
+            }
+        }
+        return caminoFinal;
+    }
+
+    private boolean dfs(Graph<String> ciudades, Vertex<String> or,
+                     Vertex<String> des, List<String> caminoActual, List<String> caminoAct
+            ,boolean[] visitados, int montoMinimo, int montoActual){
+        boolean encontrado = false;
+        visitados[or.getPosition()] = true; // Marca el origen como visitado
+        caminoActual.add(or.getData()); // Agrega el origen al recorrido
+        if(or == des){
+            if(montoActual >= montoMinimo){
+                caminoAct.clear();
+                caminoAct.addAll(caminoActual);
+                return true; // Se encontró un camino válido
+            }
+        }
+        else{
+            List<Edge<String>> adyacentes = ciudades.getEdges(or);
+            Iterator<Edge<String>> it = adyacentes.iterator();
+            while(it.hasNext() && !encontrado){
+                Edge<String> adyacente = it.next();
+                Vertex<String> vecino = adyacente.getTarget();
+                int pesoArista = adyacente.getWeight();
+                if(!visitados[vecino.getPosition()]){
+                    encontrado = dfs(ciudades, vecino, des, caminoActual, caminoAct, visitados, montoMinimo, montoActual + pesoArista);
+                }
+            }
+        }
+        if(!encontrado){
+            caminoActual.remove(caminoActual.size() - 1); // Deshace el último paso si no se encontró un camino
+        }
+        visitados[or.getPosition()] = false; // Desmarca el origen al retroceder
+        return encontrado;
+    }
+}
